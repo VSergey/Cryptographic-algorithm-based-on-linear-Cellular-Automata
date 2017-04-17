69171b0d0aea80df33624563b9233164e9c1363c
Чистобаев Андрей <kukushechkaebushechka@gmail.com>
2017-04-17 15:29:53 +0300

    Вынес определение.
    Код почти канонiчен.

--- Lib/try1/Field.hpp
+++ Lib/try1/Field.hpp
@@ -20,42 +20,46 @@ namespace Crypto
 		std::vector < bool > next;
 		const size_t length = sizeof(unsigned short) * CHAR_BIT;
 		public:
-		/*
-			Получаем номер клеточного автомата в виде объекта rule и код символа для зашифровки.
-			Устанавливаем размеры и инициализируем поле двоичными разрядами кодируемого числа.
-			Внимание! Конструктор Rule не explicit, вместо первого аргумента можно передать целое число [0;255].
-		*/
-			Field(const Rule & rule, const unsigned short code):rule(rule)
-			{
-				//ставим размеры
-				current.resize(length);
-				next.resize(length);
-				//записываем биты числа в поле (по порядку)
-				for (size_t i = 0; i < length; ++i)
-					current[length - 1 - i] = code & (1 << i);
-			}
+			/*
+				Получаем номер клеточного автомата в виде объекта rule и код символа для зашифровки.
+				Устанавливаем размеры и инициализируем поле двоичными разрядами кодируемого числа.
+				Внимание! Конструктор Rule не explicit, вместо первого аргумента можно передать целое число [0;255].
+			*/
+			Field(const Rule & rule, const unsigned short code)
 			/*
 				Считаем следующее поколение.
 				Возвращаем его в виде десятичного числа.
 			*/
 			unsigned short encrypt()
-			{
-				//пробегаем обычные клетки
-				for (size_t i = 1; i < length - 1; ++i)
-					next[i] = rule[current[i - 1] * 4 + current[i] * 2 + current[i + 1]]; //запрашиваем следующее состояние, передавая окрестность в десятичном виде
-				//обрабатываем крайние клетки
-				//замыкаем поле в кольцо
-				next[0] = rule[current[length - 1] * 4 + current[0] * 2 + current[1]];
-				next[length - 1] = rule[current[length - 2] * 4 + current[length - 1] * 2 + current[0]];
-				//теперь новое поколение стало нынешним
-				for (size_t i = 0; i < length; ++i)
-					current[i] = next[i];
-				//переводим в десятичную систему счисления и возвращаем текущее поколение
-				unsigned short code = 0;
-				for (size_t i = 0; i < length; ++i)
-					code += current[length - 1 - i] * (1 << i);
-				return code;
-			}
 	};
+}
+
+Crypto::Field::Field(const Rule & rule, const unsigned short code):rule(rule)
+{
+	//ставим размеры
+	current.resize(length);
+	next.resize(length);
+	//записываем биты числа в поле (по порядку)
+	for (size_t i = 0; i < length; ++i)
+		current[length - 1 - i] = code & (1 << i);
+}
+
+unsigned short Crypto::Field::encrypt()
+{
+	//пробегаем обычные клетки
+	for (size_t i = 1; i < length - 1; ++i)
+		next[i] = rule[current[i - 1] * 4 + current[i] * 2 + current[i + 1]]; //запрашиваем следующее состояние, передавая окрестность в десятичном виде
+	//обрабатываем крайние клетки
+	//замыкаем поле в кольцо
+	next[0] = rule[current[length - 1] * 4 + current[0] * 2 + current[1]];
+	next[length - 1] = rule[current[length - 2] * 4 + current[length - 1] * 2 + current[0]];
+	//теперь новое поколение стало нынешним
+	for (size_t i = 0; i < length; ++i)
+		current[i] = next[i];
+	//переводим в десятичную систему счисления и возвращаем текущее поколение
+	unsigned short code = 0;
+	for (size_t i = 0; i < length; ++i)
+		code += current[length - 1 - i] * (1 << i);
+	return code;
 }
 #endif
